b9facee5043399f5ba819d8d7000793477842d66
rediahmds-wsl2 <redi.ngoding@gmail.com>
2024-03-04 13:10:52 +0700

    feat: format currency

--- app/script.js
+++ app/script.js
@@ -23,11 +23,17 @@ function addExpense() {
 function updateExpensesList(name, amount) {
   const expenseList = document.getElementById("expenseList");
   const newExpenseItem = document.createElement("li");
-  newExpenseItem.textContent = `${name} - ${amount.toFixed(2)} IDR`;
+  newExpenseItem.textContent = `${name} - ${convertToIDR(amount)}`;
   expenseList.appendChild(newExpenseItem);
 }
 
 function updateTotalExpense() {
   const totalExpenseElement = document.getElementById("totalExpense");
-  totalExpenseElement.textContent = totalExpense.toFixed(2);
+  totalExpenseElement.textContent = convertToIDR(totalExpense);
 }
+
+const convertToIDR = (amount) =>
+  new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(amount);
